40cb0ce0a4fa4d1e84372b47a04cf2d6afec8c36
Rick Chadwick <rick@madeinbeta.co.uk>
2019-11-05 16:46:47 +0000

    added perch

---
+++ perch/config/config.php
@@ -0,0 +1,22 @@
+<?php
+    define('PERCH_LICENSE_KEY', 'P21407-YQW506-FNG778-FPD603-PUE023');
+
+    define("PERCH_DB_USERNAME", 'root');
+    define("PERCH_DB_PASSWORD", 'root');
+    define("PERCH_DB_SERVER", "localhost");
+    define("PERCH_DB_DATABASE", "mls");
+    define("PERCH_DB_PREFIX", "perch3_");
+    
+    define('PERCH_TZ', 'UTC');
+
+    define('PERCH_EMAIL_FROM', 'rick@madeinbeta.co.uk');
+    define('PERCH_EMAIL_FROM_NAME', 'Rick Chadwick');
+
+    define('PERCH_LOGINPATH', '/perch');
+    define('PERCH_PATH', str_replace(DIRECTORY_SEPARATOR.'config', '', __DIR__));
+    define('PERCH_CORE', PERCH_PATH.DIRECTORY_SEPARATOR.'core');
+
+    define('PERCH_RESFILEPATH', PERCH_PATH . DIRECTORY_SEPARATOR . 'resources');
+    define('PERCH_RESPATH', PERCH_LOGINPATH . '/resources');
+    
+    define('PERCH_HTML5', true);
