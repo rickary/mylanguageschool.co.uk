1f9104e69bde2fcc2433ff8dac7640218824e1d2
Rick Chadwick <rick@madeinbeta.co.uk>
2019-11-05 17:48:21 +0000

    establishing some patterns

---
+++ perch/templates/layouts/global/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  
+  <link rel="stylesheet" href="/assets/css/master.css?v0.4" />
+
+  <title><?php perch_layout_var('title'); ?></title>
+
+  <?php perch_page_attributes(); ?>
+</head>
+<body>
+  
---
+++ perch/templates/layouts/global/header.php
@@ -0,0 +1,4 @@
+<header>
+  Test
+
+</header>
