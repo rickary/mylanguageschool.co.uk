6530ea755408365b4eb4564dde7e80847bcab507
Rick Chadwick <rick@madeinbeta.co.uk>
2019-11-14 11:11:37 +0000

    open graph stuff

--- perch/templates/layouts/global/head.php
+++ perch/templates/layouts/global/head.php
@@ -8,8 +8,24 @@
   <link rel="stylesheet" href="/assets/css/master.css?v0.4" />
 
   <title><?php perch_pages_title(); ?></title>
+  
 
-  <?php perch_page_attributes(); ?>
+  <?php
+  
+  $domain        = 'http://'.$_SERVER["HTTP_HOST"];
+  $url           = $domain.$_SERVER["REQUEST_URI"];
+  $sitename      = "My Language School";
+  $twittername   = "";
+  $sharing_image = '/images/default_fb_image.jpg';
+
+  PerchSystem::set_var('domain',$domain);
+  PerchSystem::set_var('url',$url);
+  PerchSystem::set_var('sharing_image',$sharing_image);
+  PerchSystem::set_var('twittername',$twittername);
+  
+  perch_page_attributes(); 
+  
+  ?>
 </head>
 
 <body class="<?php perch_layout_var('class'); ?>">
