c8aad3a45e958b4a25fb22723f2045f68522efd9
Rick Chadwick <rick@madeinbeta.co.uk>
2019-11-20 11:15:37 +0000

    typography change

--- perch/templates/layouts/global/head.php
+++ perch/templates/layouts/global/head.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   
-  <link rel="stylesheet" href="/assets/css/master.css?v1.1" />
+  <link rel="stylesheet" href="/assets/css/master.css?v1.2" />
 
   <title><?php perch_pages_title(); ?></title>
   
