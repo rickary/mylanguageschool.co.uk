865315f4c0585b4ce1fbfe2e13dd1d61f1a3c167
Rick Chadwick <rick@madeinbeta.co.uk>
2019-11-20 10:19:04 +0000

    temp removed schools

--- perch/templates/layouts/home/services.php
+++ perch/templates/layouts/home/services.php
@@ -23,31 +23,35 @@
   </div>
 </section>
 
-<section class="l-container l-container--small l-split-alternate">
+<!-- <section class="l-container l-container--small l-split-alternate">
   <div class="l-flex-split l-flex-split--center l-flex-split--small p-services">
     <div class="l-flex-split__inner l-split-item__body">
-      <?php perch_content('Schools', [
-        'template' => 'home/services-intro.html'
-      ]); ?>
+      <?php 
+      // perch_content('Schools', [
+      //   'template' => 'home/services-intro.html'
+      // ]); 
+      ?>
 
       <?php
-        perch_pages_navigation(array(
-          'navgroup' => 'schools',
-          'template' => 'list.html',
-          'hide-extensions' => true
-        )); 
+        // perch_pages_navigation(array(
+        //   'navgroup' => 'schools',
+        //   'template' => 'list.html',
+        //   'hide-extensions' => true
+        // )); 
       ?>
     
     </div>
 
     <div class="l-flex-split__inner l-split-item__img">
-      <?php perch_content('Schools', [
-        'path' => '/',
-        'template' => 'home/services-img.html'
-      ]); ?>
+      <?php 
+      // perch_content('Schools', [
+      //   'path' => '/',
+      //   'template' => 'home/services-img.html'
+      // ]); 
+      ?>
     </div>
   </div>
-</section>
+</section> -->
 
 <section class="l-container l-container--small l-split-alternate">
   <div class="l-flex-split l-flex-split--center l-flex-split--small p-services">
