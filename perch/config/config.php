b4a5002bbe7b54ec58f5a095c986722260ca456b
Rick Chadwick <rick@madeinbeta.co.uk>
2019-11-20 09:36:02 +0000

    email settings

--- perch/config/config.php
+++ perch/config/config.php
@@ -25,8 +25,16 @@
     
     define('PERCH_TZ', 'UTC');
 
-    define('PERCH_EMAIL_FROM', 'rick@madeinbeta.co.uk');
-    define('PERCH_EMAIL_FROM_NAME', 'Rick Chadwick');
+    define('PERCH_EMAIL_FROM', 'justin@mylanguageschool.co.uk');
+    define('PERCH_EMAIL_FROM_NAME', 'Justin Lance');
+
+    define('PERCH_EMAIL_METHOD', 'smtp');
+    define('PERCH_EMAIL_HOST', 'smtp.gmail.com');
+    define('PERCH_EMAIL_AUTH', true);
+    define('PERCH_EMAIL_SECURE', 'ssl');
+    define('PERCH_EMAIL_PORT', 465);
+    define('PERCH_EMAIL_USERNAME', 'english@mylanguageschool.co.uk');
+    define('PERCH_EMAIL_PASSWORD', 'Do-you-speak-English?');
 
     define('PERCH_LOGINPATH', '/perch');
     define('PERCH_PATH', str_replace(DIRECTORY_SEPARATOR.'config', '', __DIR__));
